9539e737210eaedb3acbadc8e566a451aab618c3
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-07-22 12:50:55 -0300

    Boolean and type conversion learned

---
+++ CursoPython/boolean.py
@@ -0,0 +1,6 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa, só existem 2 repostas possiveis: sim (True) ou não (False)
+# Existem vários operadores para 'questionar'
+# Dentre eles, o ==, que é um operador lógico que questiona se um valor é igual a outro
+print(10 == 10) #Sim, verdadeiro -> True
+print(10 == 11) #Não, falso -> False
---
+++ type_convertion.py
@@ -0,0 +1,5 @@
+#   Conversão de tipos, coerção, type convertion, typecasting, coercion é o ato de converter um tipo em outro.
+# Tipos imutáveis e primitivos: str, int, float, bool
+
+print(int('1') + 1)
+print("a"+"b")
