c933b9ed9971615ca23e82ed189ab7cd08aff531
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-07-22 11:39:18 -0300

    int and float types commited

---
+++ CursoPython/int_float.py
@@ -0,0 +1,21 @@
+# Tipos int e float
+# int -> Numero inteiro
+# O tipo int representa qualquer numero
+# Positivo ou negativo, sem casas decimais.
+print(10)  # Exemplo de inteiro positivo
+print(-10)  # Exemplo de inteiro negativo
+print(0)  # Exemplo de inteiro zero
+
+
+# float -> Numero com pontos flutuantes
+# O tipo float representa qualquer numero
+# positivo ou negativo, com casas decimais.
+# float sem sinal é considerado positivo.
+print(10.5)  # Exemplo de float positivo
+print(-10.5)  # Exemplo de float negativo
+print(0.0)  # Exemplo de float zero
+
+# A função type mostra o tipo que o Python inferiu ao valor
+print(type(10))  # <class 'int'>
+print(type(10.5))  # <class 'float'>
+print(type("Vitor"))
