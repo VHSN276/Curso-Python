5a3d9bee75fd095416b6fcf1836a0c03947302d0
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-07-22 13:08:11 -0300

    Exercicios done, folder organization

---
+++ Aulas/variaveis.py
@@ -0,0 +1,11 @@
+#   Variáveis são usadas para salvar na memória do computador
+#   PEP8: inicie variáveis com letras minusculas, pode-se usar underline e numeros.
+
+nome_completo = 'Vitor Henrique'
+soma_dois_mais_dois = 2+2
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Vitor'
+idade = 19
+maior_de_idade = idade>=18
+print('Nome: ', nome, '\nIdade: ', idade, '\nÉ maior? ', maior_de_idade)
---
+++ Exercicios/variaveis_tipos_dados.py
@@ -0,0 +1,13 @@
+nome = 'Vitor'
+sobrenome = 'Henrique'
+idade = 19
+ano_nascimento = '02/07/2006'
+maior_idade = idade>=18
+altura_metros = 1.78
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de nascimento: ", ano_nascimento)
+print("É maior de idade? ", maior_idade)
+print("Altura em metros: ", altura_metros)
