8fca3e60668ee9d6aee9bbad9018f7c7d151cfc1
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-07-22 11:27:57 -0300

    First commit

---
+++ CursoPython/print.py
@@ -0,0 +1,12 @@
+
+"""
+DocString.
+"""
+
+# The command 'print()' is used to show a simple message.
+print("Olá, mundo!")
+
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, sep='-', end='\r\n')
+print(56, 78, sep="-", end='\n##')
---
+++ CursoPython/str.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos dentro de aspas simples ou duplas
+"""
+
+print(123)
+
+#Aspas simples
+print('Olá, Mundo!')
+
+#Aspas duplas
+print("Olá, Mundo!")
+
+#Escape
+print("Olá, \"Mundo!\"")
+
+#r
+print(r"Olá, \"Mundo!\"")
