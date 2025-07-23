1dea3cdb4beb2b92371dc4eae3b4918ab54f78e9
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-07-23 08:42:33 -0300

    Concat, rep, IMC, opAritmeticos done

---
+++ Aulas/concatenacao_repeticao.py
@@ -0,0 +1,7 @@
+concatenacao = 'Vitor' + ' ' +'Henrique'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_vitor = 'Vitor' * 3
+print(a_dez_vezes)
+print(tres_vezes_vitor)
---
+++ Exercicios/imc.py
@@ -0,0 +1,5 @@
+nome = 'Vitor Henrique'
+altura = 1.8
+peso = 80
+imc = peso / (altura*altura)
+print('O IMC de',nome,'é',imc)
