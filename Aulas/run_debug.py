90da5cd79eda92746ff5032132a52d6f2068f7ee
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-07-23 10:00:28 -0300

    run_debug done

---
+++ Aulas/run_debug.py
@@ -0,0 +1,14 @@
+c1 = False
+c2 = False
+c3 = True
+
+if c1 == True:
+    print("c1")
+elif c2 == True:
+    print("c2")
+elif c3 == True:
+    print("c3")
+else:
+    print("nenhum")
+
+print("fora do bloco")
