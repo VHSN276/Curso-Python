554e890865f9fee38daea19aeaa019b12df832f9
VHSN276 <vitorhsnascimento.dev@gmail.com>
2025-08-11 10:06:25 -0300

    While, Break & Continue. Done

---
+++ Aulas/while_break.py
@@ -0,0 +1,13 @@
+'''
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+'''
+n1 = 0
+flag = True
+while(flag):
+    print(n1)
+    n1+=1
+    if n1==5:
+        #flag = False
+        break 
